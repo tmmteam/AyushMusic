2311f36bf96b40cca1bd2fb256a68b7939c3a5b1
tmmteam <teamtmm2@gmail.com>
2026-01-17 19:40:54 +0530

    🥀ᴀʏᴜsʜ🥀

--- AyushMusic/__init__.py
+++ AyushMusic/__init__.py
@@ -1,33 +1,51 @@
-import os
-
 from AyushMusic.core.bot import Aru
+
 from AyushMusic.core.dir import dirr
+
 from AyushMusic.core.git import git
+
 from AyushMusic.core.userbot import Userbot
+
 from AyushMusic.misc import dbb, heroku
+
+
 
 from .logging import LOGGER
 
+
+
 dirr()
 
-# ✅ heroku / docker pe git mat chalao
-if os.getenv("DYNO") is None:
-    git()
-else:
-    LOGGER(__name__).info("git skipped (heroku detected)")
+git()
 
 dbb()
+
 heroku()
 
+
+
 app = Aru()
+
 userbot = Userbot()
+
+
+
+
 
 from .platforms import *
 
+
+
 Apple = AppleAPI()
+
 Carbon = CarbonAPI()
+
 SoundCloud = SoundAPI()
+
 Spotify = SpotifyAPI()
+
 Resso = RessoAPI()
+
 Telegram = TeleAPI()
+
 YouTube = YouTubeAPI()
